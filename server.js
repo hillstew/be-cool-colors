72c305bef1443df417defc720824e34314284484
Whitney Burton <whitneyburton9@gmail.com>
2019-03-19 16:24:49 -0600

    Move endpoints to app.js
    Co-authored-by: Hillary Stewart <hillarystewart4427@gmail.com>

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
-const express = require('express')
-const app = express();
+const app = require('./app');
 
-app.set('port', process.env.PORT || 3001)
+app.set('port', process.env.PORT || 3002);
 
 app.listen(app.get('port'), () => {
   console.log(`App is running on http://localhost:${app.get('port')}.`)
-})
+});
 
-
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
-const database = require('knex')(configuration);
-
-app.get('api/v1/project', (req, res) => {
-  database('projects').select()
-    .then(projects => res.status(200).json(projects))
-    .catch(error => res.status(500).json({ error }))
-})
+module.exports = app;
