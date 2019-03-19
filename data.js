be382797e65b9ab033f80fc248674420bb6d6716
Whitney Burton <whitneyburton9@gmail.com>
2019-03-19 16:24:00 -0600

    Add additional mock data
    Co-authored-by: Hillary Stewart <hillarystewart4427@gmail.com>

--- data.js
+++ data.js
@@ -7,8 +7,34 @@ const projects = [
         color_1: 'ffffff',
         color_2: 'ff2232',
         color_3: '2323de',
-        color_4: '89rtye',
-        color_5: '34rhg6'
+        color_4: '89fdae',
+        color_5: '34ahg6'
+      }
+    ]
+  },
+  {
+    name: 'Trapper Keeper',
+    palettes: [
+      {
+        name: 'new-theme',
+        color_1: 'ffffff',
+        color_2: 'aaaaaa',
+        color_3: '78afff',
+        color_4: '123abc',
+        color_5: 'aa33aa'
+      }
+    ]
+  },
+  {
+    name: 'SWAPIbox',
+    palettes: [
+      {
+        name: 'star-theme',
+        color_1: 'ffffff',
+        color_2: '000000',
+        color_3: '382dah',
+        color_4: '0022aa',
+        color_5: 'hhaf73'
       }
     ]
   }
